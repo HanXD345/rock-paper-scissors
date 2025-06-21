33e6ac3636cf1ba5484378dfc7d5b142e8905dd9
HanXD345 <manpreet345@yahoo.com>
2025-06-21 19:50:07 -0400

    Add rock-paper-scissors console functionality
    gameBoard —> factory function consisting of the board itself in an
    array. The IIFE will return the array (formatted) as well as a method
    to insert a chosen move onto the board.
    
    Player —> factory function consisting of the player itself, containing
    their name and their respective symbol (‘O’ or ‘X’ in this case)
    
    Cell -> factory function which holds a symbol value, and allows you to
    modify this value within the function
    
    gameController -> factory function consisting of printing player turns
    and playing rounds (game logic to be coded)

--- scripts/script.js
+++ scripts/script.js
@@ -10,35 +10,101 @@ const gameBoard = (function() {
     for (let i = 0; i < rows; i++) {
         let row = [];
         for (let j = 0; j < columns; j++) {
-            row.push(0);
+            const cell = Cell()
+            cell.changeValue("*")
+            row.push(cell)
         }
         gameBoard.push(row);
     }
 
+    console.log(gameBoard);
+
     const displayBoard = () => gameBoard;
 
     const insertChoice = (row, column, symbol) => {
-        gameBoard[row][column] = symbol;
+        if (gameBoard[row][column].getValue() !== '*') {
+            console.log("Invalid Move. There is already a symbol there.")
+            return false;
+        }
+        gameBoard[row][column].changeValue(symbol);
+        return symbol;
+    }
+
+    const printBoard = () => {
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < rows; j++){
+                console.log(gameBoard[i][j].getValue());
+            }
+        }
     }
 
     return {
         displayBoard,
         insertChoice,
+        printBoard,
     };
 })();
 
-function Player(name, symbol) {
-    const name = () => name;
-    const symbol = () => symbol;
+function Cell(symbol) {
+    const changeValue = (newSymbol) => {
+        symbol = newSymbol;
+    }
+
+    const getValue = () => symbol;
 
     return {
-        name,
-        symbol,
+        changeValue,
+        getValue,
     }
 }
 
-function gameController() {
-    ;
+function Player(name, symbol) {
+    const getName = () => name;
+    const getSymbol = () => symbol;
+
+    return {
+        getName,
+        getSymbol,
+    }
 }
 
-console.log(gameBoard.displayBoard());
+const controller = (function gameController(player1 = Player('Player One', 'X'), player2 = Player('Player Two', 'O')) {
+    let turn = 0;
+    const board = gameBoard;
+
+    const alternateTurns = (turn) => {
+        return turn === 0 ? 1 : 0;
+    };
+
+    const getPlayerTurn = () => turn;
+
+    const printPlayerTurn = (player) => {
+        console.log(board.printBoard());
+        console.log(`${player.getName()}'s Turn`);
+    }
+
+    const playRound = () => {
+        i = 0;
+        while (i < 2) {
+            printPlayerTurn(player1);
+
+            console.log("Where would you like to place the symbol? (give coordinates)")
+            const row = prompt("Enter row: ");
+            const column = prompt("Enter column: ")
+    
+            const chosen = gameBoard.insertChoice(row, column, player1.getSymbol());
+    
+            if (chosen === false) {
+                continue;
+            } else {
+                console.log("Move placed.")
+                i += 1;
+            }
+        }
+    }
+
+    return {playRound}
+})();
+
+
+controller.playRound();
