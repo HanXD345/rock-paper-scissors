f35c4b7b82627fea25dce6d4636e6d5d85e820a0
HanXD345 <manpreet345@yahoo.com>
2025-06-20 22:06:21 -0400

    Create factory functions for board and player

--- scripts/script.js
+++ scripts/script.js
@@ -3,18 +3,42 @@ const gameBoard = (function() {
     const columns = 3;
     let gameBoard = [];
 
+    // Sets up the board for the game
+    /* in the form: [[0, 0, 0],
+                     [0, 0, 0],
+                     [0, 0, 0]] */
     for (let i = 0; i < rows; i++) {
         let row = [];
-
         for (let j = 0; j < columns; j++) {
             row.push(0);
         }
         gameBoard.push(row);
     }
 
+    const displayBoard = () => gameBoard;
+
+    const insertChoice = (row, column, symbol) => {
+        gameBoard[row][column] = symbol;
+    }
+
     return {
-        gameBoard
+        displayBoard,
+        insertChoice,
     };
 })();
 
-console.log(gameBoard);
+function Player(name, symbol) {
+    const name = () => name;
+    const symbol = () => symbol;
+
+    return {
+        name,
+        symbol,
+    }
+}
+
+function gameController() {
+    ;
+}
+
+console.log(gameBoard.displayBoard());
